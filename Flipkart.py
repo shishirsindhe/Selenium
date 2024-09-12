c4cf79fdc0e3e1313815b8440252436ace9af008
shishirsindhe <shishirsindhe99@gmail.com>
2024-09-12 11:22:19 +0530

    Minor changes

--- Flipkart.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.chrome.webdriver import WebDriver as Chrome
-from selenium.webdriver.chrome.options import Options
-chrome_options=Options()
-chrome_options.add_experimental_option(name="detach",value=True)
-driver=Chrome(chrome_options)
-driver.maximize_window()
-driver.get("https://github.com")
-driver.save_screenshot("git.png")
-driver.get_screenshot_as_file("github.jpg")
